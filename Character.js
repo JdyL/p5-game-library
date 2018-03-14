0bf0f8abb54644202f5a12939e442666517541df
Jdyl <threetwoonezero@hotmail.com>
2018-03-15 00:10:10 +1100

    Character class to define

--- Character.js
+++ Character.js
@@ -3,6 +3,10 @@ function Character(posX, posY, display) {
     this.posY = posY;
     this.display = display;
 
+    this.initialize = function () {
+
+    }
+
 
     function hide() {
         this.display = false;
--- MainCharacter.js
+++ MainCharacter.js
@@ -2,36 +2,45 @@ function MainCharacter(posX, posY, display) {
     this.posX = posX;
     this.posY = posY;
     this.display = display;
-    const movementSpeed = 20;
+    const initialMovementSpeed = 7;
+    var movementSpeed = initialMovementSpeed;
+    const movementMultiplier = 2;
     mainCharacter = new Character(posX, posY, display);
+
+    const width = 50;
+    const height = 50;
+
+    const movementAfterMultiplier = movementSpeed * movementMultiplier;
 
     this.initialize = function () {
         if (this.display == true) {
             fill(255);
-            console.log(this.posX, this.posY)
-            ellipse(this.posX, this.posY, 200, 200);
+            ellipse(this.posX, this.posY, width, height); // should be the character itself
             this.keys();
         }
     }
 
     this.keys = function () {
-        // if (key == 'Z') {
-        //     console.log("You pressed Z!")
-        // }
-
+        // remove else for multidirectional - diagonal movements
         if (keyIsDown(LEFT_ARROW)) {
-            console.log("this works");
-
+            this.posX -= movementSpeed;
+        } else if (keyIsDown(RIGHT_ARROW)) {
+            this.posX += movementSpeed;
+        } else if (keyIsDown(UP_ARROW)) {
+            this.posY -= movementSpeed;
+        } else if (keyIsDown(DOWN_ARROW)) {
+            this.posY += movementSpeed;
         }
 
-    }
+        if (keyIsDown(SHIFT)) {
+            movementSpeed = movementAfterMultiplier;
+        } else {
+            movementSpeed = initialMovementSpeed;
+        }
 
-    function update_pos_X() {
-        this.posX += 20;
-    }
 
-    function update_pos_Y() {
-        this.posX += 20;
+
+
     }
 
 
